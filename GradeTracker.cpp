500d216a77c33c2abf839ef1ea1069bfdeebdea6
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-03-11 11:34:47 -0500

    Create GradeTracker.cpp

---
+++ GradeTracker.cpp
@@ -0,0 +1,36 @@
+//Grade Tracker
+#include<iostream>
+
+
+using namespace std;
+
+int main() {
+	//Create a vector of ints to store assignment grades
+	
+
+	while (true) {
+		//Display the menu
+		cout << "Choose an option:" << endl;
+		cout << "1. Add an assignment grade" << endl;
+		cout << "2. Show all assignment grades" << endl;
+		cout << "3. Give 5 point bonus" << endl;
+		cout << "4. Drop lowest assignment grade" << endl;
+		cout << "5. Display current percentage" << endl;
+		cout << "6. Exit" << endl;
+		int input;
+		cin >> input;
+
+		//Use if-statements to choose an action based on the user's choice
+    if(input == 1){
+       //Add an assignment to the vector
+       
+    }
+		
+
+		//Pause and clear screen before displaying menu again
+		cout << "\n\n";
+		system("pause");
+		system("cls");
+	}
+}
+
